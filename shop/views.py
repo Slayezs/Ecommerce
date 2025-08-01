c469a56abf1e839e6e8805646e9457ef584c3167
Chirag <chirag657krsingh@gmail.com>
2025-08-01 12:12:47 +0530

    Tracking Status updated and displaying of the prices done

--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from .models import Product,Contact,Orders
+from .models import Product,Contact,Orders,OrderUpdate
 from math import ceil
 import logging
+import json
 
 #Set up logging
 logger = logging.getLogger(__name__)
@@ -44,6 +45,23 @@ def contact(request):
     return render(request, 'shop/contact.html')
 
 def tracker(request):
+    if request.method=="POST":
+        orderId = request.POST.get('orderId', '')
+        email = request.POST.get('email', '')
+        try:
+            order = Orders.objects.filter(order_id=orderId, email=email)
+            if len(order)>0:
+                update = OrderUpdate.objects.filter(order_id=orderId)
+                updates = []
+                for item in update:
+                    updates.append({'text': item.update_desc, 'time': item.timestamp})
+                    response = json.dumps([updates, order[0].items_json], default=str)
+                return HttpResponse(response)
+            else:
+                return HttpResponse('{}')
+        except Exception as e:
+            return HttpResponse('{}')
+
     return render(request, 'shop/tracker.html')
 
 def search(request):
@@ -69,6 +87,8 @@ def checkout(request):
                        address=address,city=city,state=state,
                         zip_code=zip_code,phone=phone)
         order.save()  
+        update = OrderUpdate(order_id=order.order_id, update_desc="The order has been placed")
+        update.save()
         thank = True
         id = order.order_id
         return render(request, 'shop/checkout.html', {'thank': thank, 'id': id})
